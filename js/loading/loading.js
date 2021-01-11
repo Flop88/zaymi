73fbb595e1f4d96b6b20bb978b046d6572c5d1dc
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2021-01-12 00:13:36 +0300

    Add loading script and timer page

---
+++ js/loading/loading.js
@@ -0,0 +1,23 @@
+var body = document.querySelector('body'),
+    bar = document.querySelector('.progress-bar'),
+    counter = document.querySelector('.count'),
+    h2text = document.querySelector('.text_done'),
+    i = 0,
+    throttle = 0.9; // 0-1
+
+(function draw() {
+  if(i <= 100) {
+    var r = Math.random();
+    
+    requestAnimationFrame(draw);  
+    bar.style.width = i + '%';
+    counter.innerHTML = Math.round(i) + '%';
+    
+    if(r < throttle) { // Simulate d/l speed and uneven bitrate
+      i = i + r;
+    }
+  } else {;
+    bar.className += " done";
+    h2text.className += " done";
+  }
+})();
